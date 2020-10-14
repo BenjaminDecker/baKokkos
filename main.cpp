9d5dbc3156a4ab3fa4a1a640a343f28a51330f5b
Benjamin Decker <benjamin.decker@tum.de>
2020-10-14 17:55:51 +0200

    Kokkos implementation Test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <Kokkos_Core.hpp>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main (int argc, char *argv[]) {
+    Kokkos::initialize(argc, argv);
+    Kokkos::parallel_for( "test", 1000, KOKKOS_LAMBDA ( int i ) {
+        std::cout << i << std::endl;
+    });
+    Kokkos::finalize();
     return 0;
 }
