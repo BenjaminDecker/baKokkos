c8a3a922b93a5f3b0f14d75816ceeba8a5d57c65
Benjamin Decker <benjamin.decker@tum.de>
2020-11-08 18:18:18 +0100

    Add doxygen comments to the LinkedCellsParticleContainer class

--- LinkedCellsParticleContainer.h
+++ LinkedCellsParticleContainer.h
@@ -6,6 +6,18 @@
 
 #include "ParticleContainer.h"
 
+/**
+ * @brief Divides the simulation domain into cells, which contain particles.
+ *
+ * The cells are cubes with a sidelength of the cutoff property. This means that every particle in one cell only needs
+ * to calculate the contributing force to and from other particles within the same cell and particles inside neighboring
+ * cells. This automatically filters out many of the force calculations of particle pairs that are further away from
+ * another than the cutoff distance and can therefore be neglected.
+ * This layout improves performance if the simulation contains more than a handful of particles, as the time complexity
+ * for the force calculation grows with only O(n).
+ *
+ * @see Particle, Coord3D
+ */
 class LinkedCellsParticleContainer : public ParticleContainer {
   /// Creates a Particle from the particle information in device memory with the specified id.
   [[nodiscard]] Particle getParticle(int id) const override;
